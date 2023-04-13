4bb80c0976c34ff700db2421bcff4fa9472378da
Rasmita Dash <rasdash@publicisgroupe.net>
2023-04-13 14:30:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading= React.createElement("h1",{}, "Hello From React");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+{/* <div id= "parent">
+    <div id="child">
+        <h1>nested element!</h1>
+    </div>
+</div> */}
+
+const parent =  React.createElement("div",{id:"parent"},
+React.createElement("div", {'id': 'child'},
+React.createElement('h1',{},"nested h1 element")));
+
+root.render(parent);
