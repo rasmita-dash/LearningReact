ac4abd8a0ed9f34c13f8e02c9bb97d625b6b4576
rasmita-dash <dashrasmita.pipi@gmail.com>
2023-04-26 16:46:02 +0530

    packages

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const heading= React.createElement("h1",{}, "Hello From React");
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
@@ -9,7 +12,7 @@ root.render(heading);
 </div> */}
 
 const parent =  React.createElement("div",{id:"parent"},
-React.createElement("div", {'id': 'child'},
-React.createElement('h1',{},"nested h1 element")));
+                                        React.createElement("div", {'id': 'child'},
+                                                                React.createElement('h1',{},"nested h1 element")));
 
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LearningReact
+
+Dev Buikld
+Locval server
+HMR= Hot Module Replacement
+Uses file watching algorithm using C++
+Caching - parcel-cache - Fastyer build
+Image optimization
+Minification - for prod build, ssmaller name to vsariables & functions
+Bundling
+Compressing - removing extra spaces
+Consistent hashing
+Code Splitting
+Differential Bundling - to support older browsers
+Diagnostic
+Error handling
+HTTPs
+Tree shaking - Remove unused code
+different Dev & prod build
