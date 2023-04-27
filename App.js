c42f17b7e5503dbc648d55f9640d13649c7b7e3c
rasmita-dash <dashrasmita.pipi@gmail.com>
2023-04-27 13:46:25 +0530

    Zwigato set up

--- App.js
+++ App.js
@@ -17,5 +17,36 @@ const parent =  React.createElement("div",{id:"parent"},
 
 root.render(parent);
 
+// JSX
 const jsxHeading= <h1 id = 'header'>heading from JSX</h1>;
 root.render(jsxHeading);
+
+// Functional Component
+const HeadingComponent = () =>{
+    return <h1> Functional component</h1>
+}
+//HeadingComponent = () => <h1>Functional componet </h1>
+root.render(<HeadingComponent/>)
+
+// Component Composition
+const ParagraphComponment =() =>{
+    return <p>
+        <HeadingComponent/>
+        <label>A Javascript function that returns JSX or React element.</label>
+    </p>
+}
+root.render(<ParagraphComponment/>)
+
+// Any javascript code can be injected inside JSX within {}
+// Functional components are js functions. so they can be invoked in functional component
+const test=1000;
+const JSInFunctionalComponent= ()=>{
+    return <p> JS in Functional Component<br/>
+        {1000}<br/>
+        {78*45}
+        {console.log("logging test from Functional component")}
+        {ParagraphComponment()} 
+    </p>
+}
+root.render(<JSInFunctionalComponent/>)
+
